e3975d77b4d78b2ecef90c567f0f6f1b9b76571c
Sanjay Sharma <96072511+sanjaysharma94@users.noreply.github.com>
2022-06-19 14:06:04 +0530

    constructorFunction practice

---
+++ constructorFunc.js
@@ -0,0 +1,10 @@
+function Constructored (givenName,givenAge){
+    this.name = givenName;
+    this.age = givenAge;
+
+}
+
+let sanjaydetail = new Constructored("sanjayJi",25);
+let vijaydetail = new Constructored("vijayJi",23);
+
+console.log(sanjaydetail,vijaydetail)
