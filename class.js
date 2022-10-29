407f6e0497b72f493536587396ae90ed98cce1e5
Sanjay Sharma <96072511+sanjaysharma94@users.noreply.github.com>
2022-10-29 17:03:36 +0530

    Create class.js

---
+++ class.js
@@ -0,0 +1,29 @@
+ class cars {
+    constructor(name,lastname){
+        this.name=name;
+        this.lastname=lastname;
+    }
+
+    fullname= function() {
+        return this.name+" "+ this.lastname
+    }
+
+
+}
+
+class cars1 {
+    constructor(name,lastname){
+        this.name=name;
+        this.lastname=lastname;
+    }
+
+    fullname1= function(city) {
+        return this.name + " "+ this.lastname + " is from " + city
+    }
+
+
+}
+
+let first = new cars("hero","sharma");
+
+console.log(first.fullname());
